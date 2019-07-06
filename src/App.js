96e0593b8f46dc3038b4103ed2dbab9dca12d862
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-07-06 17:59:29 +0100

    removed a bunch of dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import {
+  Stitch,
+  RemoteMongoClient,
+  AnonymousCredential,
+} from 'mongodb-stitch-browser-sdk';
 import Header from './components/Header';
 import Home from './pages/Tournaments';
 import Create from './pages/CreateAI';
+
+
+const client = Stitch.initializeDefaultAppClient('aivsai-cjsto');
+
+const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('vs');
+
+client.auth.loginWithCredential(new AnonymousCredential()).then(user => db.collection('ai').updateOne({ owner_id: client.auth.user.id }, { $set: { number: 42 } }, { upsert: true })).then(() => db.collection('ai').find({ owner_id: client.auth.user.id }, { limit: 100 }).asArray()).then((docs) => {
+  console.log('Found docs', docs);
+  console.log('[MongoDB Stitch] Connected to Stitch');
+})
+  .catch((err) => {
+    console.error(err);
+  });
 
 function App() {
   return (
